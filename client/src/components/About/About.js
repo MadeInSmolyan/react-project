596085aa6b3c5e8418c428e112054e70cd15de06
MadeInSmolyan <79079476+MadeInSmolyan@users.noreply.github.com>
2021-04-19 13:47:26 +0300

    map style

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,19 +1,19 @@
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import styles from "./About.module.css";
 
-
 const About = (props) => {
   const mapStyles = {
-    width: "100%",
+    width: "900px",
     height: "300px",
-    position: "bottom",
+    marginLeft: "270px",
+    marginBottom: "0px",
   };
   return (
     <div>
       <h1 className={styles.h1}>About Us:</h1>
       <h2 className={styles.h2}>Contacts: +359 876 162 930</h2>
       <h3 className={styles.h3}>Information:</h3>
-      <p className={styles.paragraph}> 
+      <p className={styles.paragraph}>
         Whether you’ve been searching for a deal on a car like the the ones we
         offer, head over to Uni Motors,
         <br /> the Houston area’s premier dealership. Shoppers in Spring and The
